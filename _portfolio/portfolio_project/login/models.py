9af932d14b1711f067746bfc984d1eb404f1b243
Shashwat Singh <shashwat5013@gmaiil.com>
2020-05-08 12:09:57 +0530

    login page is done and data is travelling from login page to portfolio page

--- _portfolio/portfolio_project/login/models.py
+++ _portfolio/portfolio_project/login/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class REGISTERED_USER_DETAIL(models.Model):
-    registered_uesr_name=models.CharField(primary_key=True,max_length=50)
-    registered_uesr_password=models.CharField(max_length=50)
-        
+    registered_uesr_name=models.CharField(primary_key=False,max_length=50)
+    registered_uesr_password=models.CharField(primary_key=True,max_length=50)
+    registered_uesr_bio=models.CharField(max_length=200,default="You haven't entered any detail ")
+    registered_uesr_email= models.EmailField(max_length=254,default="You_Haven't_entered_mail@pleaseDo.com" )
--- _portfolio/portfolio_project/login/views.py
+++ _portfolio/portfolio_project/login/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from login.models import REGISTERED_USER_DETAIL
+from django.contrib import messages
 # Create your views here.
 def login(request):
     return render(request,'LOGIN/index.html')
 def logging_in(request):
-    print(request.POST['password'])
-    return render(request,'PORTFOLIO/index.html')
+    password_entered=request.POST['password']
+    try:
+        print(password_entered)
+        detail_of_user=REGISTERED_USER_DETAIL.objects.get(registered_uesr_password=password_entered)
+        print(detail_of_user.registered_uesr_bio)
+        return render(request,'PORTFOLIO/index.html',{"user":detail_of_user})
+    except:
+        messages.info(request,"Sorry you have entered wrong password!!! try again")
+        return render(request,'LOGIN/index.html')
